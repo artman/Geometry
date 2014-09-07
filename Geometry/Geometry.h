cd0c15549f320728d916a123d985b2d74082116b
Tuomas Artman <tuomas@artman.fi>
2014-09-07 16:18:20 -0700

    Initial Commit

---
+++ Geometry/Geometry.h
@@ -0,0 +1,19 @@
+//
+//  Geometry.h
+//  Geometry
+//
+//  Created by Tuomas Artman on 7.9.2014.
+//  Copyright (c) 2014 Tuomas Artman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Geometry.
+FOUNDATION_EXPORT double GeometryVersionNumber;
+
+//! Project version string for Geometry.
+FOUNDATION_EXPORT const unsigned char GeometryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geometry/PublicHeader.h>
+
+
