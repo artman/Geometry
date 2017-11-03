577fa5df1cca9cf91ea85d2270cfb8efdb4e61f8
Tuomas Artman <tuomas@uber.com>
2017-11-02 20:47:12 -0700

    Fixed headers

--- Geometry/Geometry.h
+++ Geometry/Geometry.h
@@ -2,14 +2,10 @@
 //  Copyright (c) 2014-2017 Tuomas Artman. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Geometry.
 FOUNDATION_EXPORT double GeometryVersionNumber;
 
 //! Project version string for Geometry.
 FOUNDATION_EXPORT const unsigned char GeometryVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Geometry/PublicHeader.h>
-
-
